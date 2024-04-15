8e1c65d32b09784427b8dfeed867b15c3d7481e2
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2024-04-15 23:16:16 +0200

    improve layout and protect against exceptions

--- src/1_Home.py
+++ src/1_Home.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="PolyKin | Home",
+    page_icon="🧮",
+    # layout="wide",
+    initial_sidebar_state="expanded")
+
 st.title("Welcome to PolyKin Web! 👋")
 st.markdown(
     "On-line polymer reaction engineering tools powered by [PolyKin](https://hugomvale.github.io/polykin/).")
